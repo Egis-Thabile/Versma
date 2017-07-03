5cdf0740b601b6c94c682d0b7da7e121feb7ac86
Egis-Thabile <thabile@egis-software.com>
2017-07-03 09:30:32 +0200

    Versma - Small code refactorting

--- resources/System/scripts/MoveToQueue.groovy
+++ resources/System/scripts/MoveToQueue.groovy
@@ -1,19 +1,20 @@
-def path = doc.node.node.parentNode.fullPath
+def parentPath = doc.node.node.parentNode.fullPath
+def Amendment_Group = doc.workflow().get('Amendment_Group')
 
-if (doc.workflow().get('Amendment_Group') == "Amendments Commercial") {
-    doc.moveTo(path.substring(0, path.lastIndexOf('/')) + "/" + doc.workflow().get('Amendment_Group') + "/" + "Commercial Amend Docs")
+if ( Amendment_Group == "Amendments Commercial") {
+    doc.moveTo(parentPath + "/" + Amendment_Group + "/" + "Commercial Amend Docs")
 }
 
-if (doc.workflow().get('Amendment_Group') == "Amendments Domestic") {
-    doc.moveTo(path.substring(0, path.lastIndexOf('/')) + "/" + doc.workflow().get('Amendment_Group') + "/" + "Domestic Amend Docs")
+if (Amendment_Group == "Amendments Domestic") {
+    doc.moveTo(parentPath + "/" + Amendment_Group + "/" + "Domestic Amend Docs")
 }
 
-if (doc.workflow().get('Amendment_Group') == "Amendments Santam Commercial") {
-    doc.moveTo(path.substring(0, path.lastIndexOf('/')) + "/" + doc.workflow().get('Amendment_Group') + "/" + "Santam Commercial Amend Docs")
+if (Amendment_Group == "Amendments Santam Commercial") {
+    doc.moveTo(parentPath + "/" + Amendment_Group + "/" + "Santam Commercial Amend Docs")
 }
 
-if (doc.workflow().get('Amendment_Group') == "Amendments Santam Domestic") {
-    doc.moveTo(path.substring(0, path.lastIndexOf('/')) + "/" + doc.workflow().get('Amendment_Group') + "/" + "Santam Domestic Amend Docs")
+if (Amendment_Group == "Amendments Santam Domestic") {
+    doc.moveTo(parentPath + "/" + Amendment_Group + "/" + "Santam Domestic Amend Docs")
 }
 
 doc.save()
--- resources/System/scripts/existingItemScript.groovy
+++ resources/System/scripts/existingItemScript.groovy
@@ -5,7 +5,7 @@ import com.egis.Session
 Session s = Kernel.get(Session.class);
 
 def path = doc.node.node.parentNode.fullPath
-def pathUp = path.substring(0, path.lastIndexOf('/'))
+def parentPath = doc.node.node.parentNode.parentNode.fullPath
 def parentNode = path.split('/')[-1]
 def supDocNode = ''
 
@@ -25,7 +25,7 @@ if (parentNode == 'Amendments Santam Domestic') {
     supDocNode = 'Santam Domestic Amend Docs'
 }
 
-def existingItem = s.getDocument("SELECT Issue_No FROM '" + pathUp + '/' + supDocNode + "' WHERE Issue_No='"+doc.workflow().get('Issue_No')+"'")
+def existingItem = s.getDocument("SELECT Issue_No FROM '" + parentPath + '/' + supDocNode + "' WHERE Issue_No='"+doc.workflow().get('Issue_No')+"'")
 
 ValidationUtils.notNull(existingItem, "Please Link Existing Items")
 
